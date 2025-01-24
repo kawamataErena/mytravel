676abdd11763715ec43526eaba44beeb676639e8
erena <eretan1233@docomo.ne.jp>
2025-01-24 13:49:53 +0000

    変更

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -21,14 +21,14 @@
         <div id="app">
             <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
                 <div class="container">
-                    <a class="navbar-brand" href="{{ url('/')}}">
+                    <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsupportedcontent" aria-controls="navbarsupportedcontent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarsupportedcontent">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- left side of navbar -->
                         <ul class="navbar-nav ms-auto">
 
