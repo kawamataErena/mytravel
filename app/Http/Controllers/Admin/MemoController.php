604ba582f6e515ae6e71cf15aaec0acda91fcc91
erena <eretan1233@docomo.ne.jp>
2025-02-18 01:27:18 +0000

    メモ追加

--- app/Http/Controllers/Admin/MemoController.php
+++ app/Http/Controllers/Admin/MemoController.php
@@ -30,5 +30,16 @@ class MemoController extends Controller
 
             return redirect('admin/memo/create');
         }
+
+        public function index(Request $request)
+        {
+            $cond_title = $request->cond_title;
+            if ($cond_title != null) {
+                $posts = Memo::where('title',$cond_title)->get();
+            } else{
+                $posts = Memo::all();
+            }
+            return view('admin.memo.index',['posts' =>$posts,'cond_title'=> $cond_title]);
+        }
 }
 
